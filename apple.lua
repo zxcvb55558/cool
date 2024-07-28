414c41ec42b73d249d29ecaa773362a7ea2ad049
qingyu <144498182+zxcvb55558@users.noreply.github.com>
2024-07-28 16:58:48 +0800

    Update apple.lua

--- apple.lua
+++ apple.lua
@@ -1,36 +1,39 @@
 local library = loadstring(game:HttpGet("https://github.com/GoHamza/AppleLibrary/blob/main/main.lua?raw=true"))()
 
 
-local window = library:init("公告", true, Enum.KeyCode.RightShift, true)
+local window = library:init("🇨🇳公告", true, Enum.KeyCode.RightShift, true)
 
 local sectionA = window:Section("公告")
 
-sectionA:Divider("🇨🇳云制作!")
-
 sectionA:Button("点击!", function()
-   print("点击按钮")
-end)     
-sectionA:Label("本脚本处于测试阶段.")
+   print("点击按钮.")
+end)
+
+sectionA:Label("本脚本永久免费.")
+
+sectionA:Switch("滑动!", false, function(a)
+   print(a)
+end)
 
 sectionA:TextField("输入文本!", "Enter text here...", function(a)
    print(a)
 end)
 
-local sectionB = window:Section("通用🏀")
+local sectionB = window:Section("🏀通用")
 
-sectionB:Divider("群主 are cool!")
+sectionB:Divider("群主💰 are cool!")
 
 sectionB:Button("Temporary Notification", function()
    window:TempNotify("小心!", "We are going to beat you up.", "rbxassetid://12608259004")
 end)
 
-sectionB:Button("测试 1", function() window:Notify("Hello!", "I am notification", "Button1", "rbxassetid://12608259004",
+sectionB:Button("Notification 1", function() window:Notify("Hello!", "I am notification", "Button1", "rbxassetid://12608259004",
    function()
        print(1)
    end)
 end)
 
-sectionB:Button("测试 2", function() window:Notify2("Hello!", "I am notification", "Button 1", "Button 2", "rbxassetid://12608259004",
+sectionB:Button("Notification 2", function() window:Notify2("Hello!", "I am notification", "Button 1", "Button 2", "rbxassetid://12608259004",
    function()
        print(1)
    end,
@@ -40,7 +43,7 @@ sectionB:Button("测试 2", function() window:Notify2("Hello!", "I am notificati
 end)
 
 window:GreenButton(function()
-   print("点击绿色按钮!")
+   print("You clicked the green button!")
 end)
 
 sectionA:Select()
