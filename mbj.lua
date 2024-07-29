f96ea07006f6a21a90992a082ffa052de99a6c7e
qingyu <144498182+zxcvb55558@users.noreply.github.com>
2024-07-29 13:55:47 +0800

    Create mbj.lua

---
+++ mbj.lua
@@ -0,0 +1,343 @@
+local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "确认身份",
+    Text = "正在验证....",
+    Duration = 3, 
+})
+
+local a=tostring(game.Players.LocalPlayer.Character);
+
+if a=="useranewrff"then
+_G.she=true
+elseif a=="lovhkun"then
+_G.she=true
+elseif a=="xiaochenbz"then
+_G.she=true
+elseif a=="qwert111treyheh"then
+_G.she=true
+elseif a=="useranewrffl"then
+_G.she=true
+elseif a=="useranewrffli"then
+_G.she=true
+elseif a=="sjejjehdhdbdhdjsk"then
+_G.she=true
+elseif a=="FangYijian"then
+_G.she=true
+elseif a=="hcuuhfc"then
+_G.she=true
+elseif a=="15_36417"then
+_G.she=true
+elseif a=="dulianhui"then
+_G.she=true
+elseif a=="123yiuopfb"then
+_G.she=true
+elseif a=="Leisure2008"then
+_G.she=true
+elseif a=="aqawr_5"then
+_G.she=true
+elseif a=="zkrwxy520"then
+_G.she=true
+elseif a=="liukcnm"then
+_G.she=true
+elseif a=="xjzjssi"then
+_G.she=true
+elseif a=="mmmkill11"then
+_G.she=true
+elseif a=="UDHFIEGRRG"then
+_G.she=true
+elseif a=="123fa98"then
+_G.she=true
+elseif a=="1234567899aaw"then
+_G.she=true
+elseif a=="cheems070828"then
+_G.she=true
+end
+if _G.she==true then
+    local CoreGui = game:GetService("StarterGui")
+
+CoreGui:SetCore("SendNotification", {
+    Title = "验证结果",
+    Text = a.."验证身份成功",
+    Duration = 5,
+})
+
+wait(3.5)
+  
+  local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "☁️脚本",
+    Text = "你没有加入白名单，即将被踢出",
+    Duration = 5, 
+})
+wait(2)
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "☁️脚本",
+    Text = "三",
+    Duration = 5, 
+})
+wait(1)
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "☁️脚本",
+    Text = "二",
+    Duration = 5, 
+})
+wait(1)
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "☁️脚本",
+    Text = "一",
+    Duration = 5, 
+})
+wait(1)
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "☁️脚本",
+    Text = "骗你的，正在启动",
+    Duration = 5, 
+})
+wait(2)
+print("Anti Afk On")
+		local vu = game:GetService("VirtualUser")
+		game:GetService("Players").LocalPlayer.Idled:connect(function()
+		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		   wait(1)
+		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		end)
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "反挂机已自动开启",
+    Text = "被踢出服务器的概率已降低",
+    Duration = 5,
+})
+wait(2)
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "启动成功",
+    Text = "您好"..game.Players.LocalPlayer.Name.."，欢迎使用脚本",
+    Duration = 5,
+})
+
+local LBLG = Instance.new("ScreenGui", getParent)
+local LBL = Instance.new("TextLabel", getParent)
+local player = game.Players.LocalPlayer
+
+LBLG.Name = "LBLG"
+LBLG.Parent = game.CoreGui
+LBLG.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+LBLG.Enabled = true
+LBL.Name = "LBL"
+LBL.Parent = LBLG
+LBL.BackgroundColor3 = Color3.new(1, 1, 1)
+LBL.BackgroundTransparency = 1
+LBL.BorderColor3 = Color3.new(0, 0, 0)
+LBL.Position = UDim2.new(0.75,0,0.010,0)
+LBL.Size = UDim2.new(0, 133, 0, 30)
+LBL.Font = Enum.Font.GothamSemibold
+LBL.Text = "TextLabel"
+LBL.TextColor3 = Color3.new(1, 1, 1)
+LBL.TextScaled = true
+LBL.TextSize = 14
+LBL.TextWrapped = true
+LBL.Visible = true
+
+local FpsLabel = LBL
+local Heartbeat = game:GetService("RunService").Heartbeat
+local LastIteration, Start
+local FrameUpdateTable = { }
+
+local function HeartbeatUpdate()
+	LastIteration = tick()
+	for Index = #FrameUpdateTable, 1, -1 do
+		FrameUpdateTable[Index + 1] = (FrameUpdateTable[Index] >= LastIteration - 1) and FrameUpdateTable[Index] or nil
+	end
+	FrameUpdateTable[1] = LastIteration
+	local CurrentFPS = (tick() - Start >= 1 and #FrameUpdateTable) or (#FrameUpdateTable / (tick() - Start))
+	CurrentFPS = CurrentFPS - CurrentFPS % 1
+	FpsLabel.Text = ("北京时间:"..os.date("%H").."时"..os.date("%M").."分"..os.date("%S"))
+end
+Start = tick()
+Heartbeat:Connect(HeartbeatUpdate)
+local CoreGui = game:GetService("StarterGui")
+CoreGui:SetCore("SendNotification", {
+    Title = "☁️脚本",
+    Text = "加载成功，欢迎！！！",
+    Duration = 5, 
+})
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "☁️脚本", HidePremium = false, SaveConfig = true, ConfigFolder = "☁️脚本"})
+
+OrionLib:MakeNotification({
+	Name = "☁️中心",
+	Content = "欢迎使用☁️脚本！",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+local Tab = Window:MakeTab({
+	Name = "☁️制作",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "☁️脚本",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+Tab:AddButton({
+	Name = "复制QQ",
+	Callback = function()
+     setclipboard("114514☁️")
+  	end
+})
+Tab:AddToggle({
+	Name = "This is a ☁️脚本!",
+	Default = false,
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+Tab:AddSlider({
+	Name = "打赏",
+	Min = 50,
+	Max = 200,
+	Default = 50,
+	Color = Color3.fromRGB(255,255,255),
+	Increment = 1,
+	ValueName = "元",
+	Callback = function(Value)
+		print(Value)
+	end    
+})
+
+local Tab = Window:MakeTab({
+        Name = "🇨🇳脚本公告🇨🇳",
+        Icon = "rbxassetid://7734068321",
+        PremiumOnly = false
+})
+
+Tab:AddParagraph("作者","🏀管理员")
+Tab:AddLabel("作者QQ：☁️")
+Tab:AddLabel("QQ群：☁️")
+Tab:AddLabel("此脚本完全免费")
+Tab:AddLabel("脚本免费脚本 请勿去圈钱","")
+Tab:AddLabel("我是个蔡坤。","")
+Tab:AddLabel("还不快双击加关注。","")
+
+local Tab = Window:MakeTab({
+	Name = "⭐通用",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "飞行",
+	Callback = function()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
+      		print("button pressed")
+  	end    
+})
+Tab:AddTextbox({
+	Name = "移动速度",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
+	end	 
+})
+
+ Tab:AddTextbox({
+	Name = "跳跃高度",
+	Default = "",
+	TextDisappear = true,
+	Callback = function(Value)
+		game.Players.LocalPlayer.Character.Humanoid.JumpPower = Value
+	end	 
+})
+Tab:AddButton({
+	Name = "飞车",
+	Callback = function()
+	--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+loadstring(game:HttpGet("https://pastebin.com/raw/GRp9rP98"))()
+      		print("button pressed")
+  	end    
+})
+Tab:AddButton({
+	Name = "动画中心",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Animation-Hub/main/Animation%20Gui", true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "修改玩家碰撞箱",
+	Callback = function()
+loadstring(game:HttpGet('https://pastebin.com/raw/JYFXjEVh'))()
+end
+})
+Tab:AddButton({
+	Name = "通用ESP",
+	Callback = function()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/Lucasfin000/SpaceHub/main/UESP'))()
+	end
+})
+Tab:AddButton({
+    Name = "HUA 光影",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/arzRCgwS"))()
+    end
+})
+
+Tab:AddButton({
+    Name = "光影_2",
+    Default = false,
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MZEEN2424/Graphics/main/Graphics.xml"))()
+    end
+})
+
+Tab:AddButton({
+    Name = "超高画质",
+    Callback = function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/jHBfJYmS"))()
+    end
+})
+Tab:AddToggle({
+	Name = "夜视",
+	Default = false,
+	Callback = function(Value)
+		if Value then
+		    game.Lighting.Ambient = Color3.new(1, 1, 1)
+		else
+		    game.Lighting.Ambient = Color3.new(0, 0, 0)
+		end
+	end
+})
+Tab:AddButton({
+    Name = "穿墙(无拉回)",
+    Callback = function()
+    loadstring(game:HttpGet("https://github.com/DXuwu/OK/raw/main/clip"))()
+    end
+})
+Tab:AddButton({
+    Name = "变身",
+    Callback = function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ChronoAccelerator/Public-Scripts/main/Morphing/MorphScript.lua"))();
+    end
+})
+Tab:AddButton({
+	Name = "辅助脚本",
+	Callback = function()
+loadstring(game:HttpGet(('https://pastebin.com/raw/R8QMbhzv')))()
+end
+})
+Tab:AddButton({
+	Name = "指令",
+	Callback = function()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end
+})
